6974748fe6baee236a2c21c4b52c67456a129fd1
Bhanu Prabhath <70201186+bhanu7410@users.noreply.github.com>
2025-11-27 17:54:21 +0530

    prisma completed

--- app/page.js
+++ app/page.js
@@ -1,20 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-	return (
-		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-background">
-			<div className="flex flex-col items-center justify-center">
-				<div className="font-bold text-5xl mb-4">
-					Personal Finance Tracker
-				</div>
-				<div className="">
-					Check & Update your finances Daily and Track your group
-					financial status
-				</div>
-				<div className="px-4 py-2 bg-blue-300 rounded text-black hover:bg-blue-400 font-bold cursor-pointer ease-in">
-					Sign In To let&apos;s get started
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="dark:bg-background flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+            <div className="flex flex-col items-center justify-center">
+                <div className="mb-4 text-5xl font-bold">
+                    Personal Finance Tracker
+                </div>
+                <div className="">
+                    Check & Update your finances Daily and Track your group
+                    financial status
+                </div>
+                <div className="mt-4 cursor-pointer rounded bg-blue-300 px-4 py-2 font-bold text-black ease-in hover:bg-blue-400">
+                    <Link href="/signin">Sign In</Link>
+                </div>
+            </div>
+        </div>
+    );
 }
