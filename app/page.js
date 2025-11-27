579def2446f81f00303c4a99f305fdc628c8bfc2
Bhanu Prabhath <70201186+bhanu7410@users.noreply.github.com>
2025-11-27 20:42:57 +0530

    log in page created

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="dark:bg-background flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-zinc-900 dark:text-white">
             <div className="flex flex-col items-center justify-center">
                 <div className="mb-4 text-5xl font-bold">
                     Personal Finance Tracker
@@ -13,7 +13,7 @@ export default function Home() {
                     financial status
                 </div>
                 <div className="mt-4 cursor-pointer rounded bg-blue-300 px-4 py-2 font-bold text-black ease-in hover:bg-blue-400">
-                    <Link href="/signin">Sign In</Link>
+                    <Link href="/sign-in">Sign In</Link>
                 </div>
             </div>
         </div>
